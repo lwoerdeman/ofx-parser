74e91d95339c1388c4c6afbfda2f34a25cac889e
Landon Woerdeman <13247759+lwoerdeman@users.noreply.github.com>
2020-09-03 17:05:58 -0500

    Make transaction converter null safe

--- ofx-server/src/main/java/com/landonwoerdeman/server/converter/TransactionConverter.java
+++ ofx-server/src/main/java/com/landonwoerdeman/server/converter/TransactionConverter.java
@@ -9,9 +9,13 @@ import java.time.ZoneOffset;
 
 public class TransactionConverter {
     public static Transaction convert(StatementTransaction statementTransaction) {
+        if (statementTransaction == null)
+            return null;
         Transaction transaction = new Transaction();
         transaction.setAmount(statementTransaction.getAmount());
-        transaction.setDatePosted(statementTransaction.getDatePosted().toInstant().toEpochMilli());
+        if (statementTransaction.getDatePosted() != null) {
+            transaction.setDatePosted(statementTransaction.getDatePosted().toInstant().toEpochMilli());
+        }
         transaction.setFitId(statementTransaction.getFitId());
         transaction.setMemo(statementTransaction.getMemo());
         transaction.setPayee(statementTransaction.getPayee());
@@ -20,13 +24,19 @@ public class TransactionConverter {
     }
 
     public static StatementTransaction convert(Transaction transaction) {
-        return new TransactionBuilder()
-                .amount(transaction.getAmount().toString())
-                .datePosted(Instant.ofEpochMilli(transaction.getDatePosted()).atOffset(ZoneOffset.UTC))
+        if (transaction == null)
+            return null;
+        TransactionBuilder builder = new TransactionBuilder()
                 .fitId(transaction.getFitId())
                 .memo(transaction.getMemo())
                 .payee(transaction.getPayee())
-                .type(transaction.getType())
-                .build();
+                .type(transaction.getType());
+        if (transaction.getAmount() != null) {
+            builder.amount(transaction.getAmount().toString());
+        }
+        if (transaction.getDatePosted() != null) {
+            builder.datePosted(Instant.ofEpochMilli(transaction.getDatePosted()).atOffset(ZoneOffset.UTC));
+        }
+        return builder.build();
     }
 }
